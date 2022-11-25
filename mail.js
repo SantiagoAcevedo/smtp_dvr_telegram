7fbba990857f76da736a70468f7b91919a014b47
Santiago Acevedo <perez320@gmail.com>
2022-11-25 12:07:15 -0300

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,74 @@
+var fs = require('fs');
+const https = require('https');
+const {SMTPServer} = require('smtp-server');
+const parser = require("mailparser").simpleParser
+const { Telegraf } = require('telegraf')
+
+const BOT_TOKEN = 'SEU TOKEN';
+const chatId = 'SEU TELEGRAM CHAT ID'; //id do canal
+
+const app = new Telegraf(BOT_TOKEN);
+
+console.log('Sistema Iniciado!')
+app.telegram.sendMessage(chatId, 'BOT INICIADO!');
+
+const server = new SMTPServer({
+    disabledCommands: ['STARTTLS'],
+    authMethods: ['PLAIN', 'LOGIN', 'CRAM-MD5'],
+    logger: false,
+    onAuth(auth, session, callback) {
+        let username = 'santiago';
+        let password = 'santiago';
+
+        if (
+            auth.username === username &&
+            (auth.method === 'CRAM-MD5'
+                ? auth.validatePassword(password) 
+                : auth.password === password)
+        ) {
+            return callback(null, {
+                user: 'userdata'
+            });
+        }
+
+        return callback(new Error('Falha ao autenticar'));
+    },
+    onData(stream, session, callback){
+        parser(stream, {}, (err, parsed) => {
+            function myFunction(item) {
+                fs.writeFile('img/'+item.filename, item.content, (err) => {
+                    if (err)
+                        console.log(err);
+                    else {
+                        console.log("Arquivo "+item.filename+" salvo\n");                      
+						app.telegram.sendPhoto(chatId, { source: 'img/'+item.filename })
+						fs.unlink('img/'+item.filename, (err) => {
+						if (err) {
+							console.error(err)
+							return
+						  }
+						})
+                    }
+                });				
+            }
+            if (err)
+                console.log("Error:" , err)
+				console.log(parsed)
+            let fix = encodeURI(parsed.text);
+            if(parsed.attachments.length > 0){
+                parsed.attachments.forEach(myFunction)
+                let envia = app.telegram.sendMessage(chatId, parsed.text);
+				console.log(envia);
+            }else{
+                let envia = app.telegram.sendMessage(chatId, parsed.text);
+				console.log(envia);
+			}	
+
+            console.log('DEBUG::: '+parsed.subject)
+            stream.on("end", callback)
+        })
+        stream.pipe(process.stdout);
+        stream.on('end', callback);
+    },
+});
+server.listen(587, 'SEU IP');
